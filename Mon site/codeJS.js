5b9a667c6a45219311bcb4626558ada948fe1027
skergoat <skergoatweb@gmail.com>
2020-05-21 09:03:33 +0700

    correction slider

--- Mon site/codeJS.js
+++ Mon site/codeJS.js
@@ -4,113 +4,79 @@ var slide = new Array('Images/slider-image1.jpg', 'Images/slider-image2.jpg', 'I
 
 var numero = 1;
 
-function ChangeSlide(sens) {
+// function ChangeSlide(sens) {
 
-    numero = numero + sens;
+//     // hide pictures  
+//     numero = numero + sens;
 
-    if (numero < 0) {
+//     if (numero < 0) {
+//         numero = slide.length - 1;
+//     }
 
-        numero = slide.length - 1;
-    }
+//     if (numero > slide.length - 1) {
+//         numero = 0;
+//     }
 
-    if (numero > slide.length - 1) {
+//     document.getElementById('slide').style.opacity = 0;
 
-        numero = 0;
-    }
-
-    
-
-     document.getElementById('slide').style.opacity = 0;
-
-for (var i = 0 ; i < 1 ; i += 0.25) {
-
-    
-
-    setTimeout(function opaciteSlider() {
-
-        document.getElementById('slide').style.opacity = i;
-
-        console.log('test2');
-
-    }), 100;
-
-    opaciteSlider();
-
-  
-
-}
-
-   
-
-    document.getElementById("slide").src = slide[numero];
+//     show pictures 
+//     for (var i = 0 ; i < 1 ; i += 0.25) {
+//         alert(i);
 
 
-}
+//         setTimeout(function opaciteSlider() {
+//             document.getElementById('slide').style.transition = "opacity 0.5s ease-in-out";
+//             document.getElementById('slide').style.opacity = i;
+//         }), 100;
 
+//         opaciteSlider();
+//     }
+//     document.getElementById("slide").src = slide[numero];
+// }
 
+/**
+ * j'ai remplace "src" par background. 
+ * Si tu change le src, il n'y aura pas de transition car ca n'est pas une qualite css
+ * or le background en est une
+ */
+// document.getElementById("slide").style.backgroundImage = "url('" + slide[0] + "')";
+// document.getElementById("slide").style.transition = "2s";
 
-
-
-
-
+// auto slide 
 function ChangeSlideTimer() {
 
    setInterval(() => {
-       
-    console.log(numero);
+        console.log(numero);
+        /**
+         * dans ce cas utilise plutot "switch" que "if...else" 
+         */
+        switch(numero) {
+            case 0:
+            case 1:
+            numero++;
+            document.getElementById("slide").style.backgroundImage = "url('" + slide[numero] + "')";
+            break;
 
+            case 2:
+            numero = 0;
+            document.getElementById("slide").style.backgroundImage = "url('" + slide[numero] + "')";
+            break;
 
-    if (numero == 0) {
-
-        numero++;
-        document.getElementById("slide").src = slide[numero];
-
-  
-    } else if (numero == 1) {
-
-        numero++;
-        document.getElementById("slide").src = slide[numero];
-
-    } else if (numero == 2) {
-
-        numero = 0;
-        document.getElementById("slide").src = slide[numero];
-
-    } else {
-
-        alert('Erreur dû au slider.')
-    }
-
-
-
-   }, 10000);
-
-
-
-
+            default: 
+            alert('Erreur dû au slider.');
+        }
+    }, 10000);
 }
 
 ChangeSlideTimer();
 
-
-
-
-
 /* TEST */
 
-
-var test = document.getElementById('test');
-
-var paragraphe = document.querySelector('.para');
-
-console.log(test, paragraphe);
-
-paragraphe.addEventListener('click', function() {
-
-    paragraphe.classList.toggle('testvalide');
-
-});
-
-
+// var test = document.getElementById('test');
+// var paragraphe = document.querySelector('.para');
+// console.log(test, paragraphe);
+// paragraphe.addEventListener('click', function() {
+//     paragraphe.classList.toggle('testvalide');
+// });
 
 // LE TEST A POUR BUT DE CHANGER LE SLIDER D'UNE BELLE FACON ...
